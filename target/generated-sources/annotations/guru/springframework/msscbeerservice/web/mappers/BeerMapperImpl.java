66631a9fa6db699ce557a1d40f3db2f881a1e8ff
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-08-30 09:03:50 +0900

    Enhance Order Line DTO with Beer Data from Beer Service. closes #4

---
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-08-30T09:00:00+0900",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Debian)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
